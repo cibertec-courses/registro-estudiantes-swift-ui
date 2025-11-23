1cf63e393ace648a0ed15ba00eeebef367b7928a
Alberth Ruiz <ch.alb.rz@hotmail.com>
2025-11-23 10:36:41 -0500

    Initial Commit

---
+++ RegistroEstudiantesSUI/RegistroEstudiantesSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegistroEstudiantesSUIApp.swift
+//  RegistroEstudiantesSUI
+//
+//  Created by Christiam Alberth Mendoza Ruiz on 23/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RegistroEstudiantesSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
